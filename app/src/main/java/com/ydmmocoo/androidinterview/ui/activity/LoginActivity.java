208c2244c456eb240daffe6d4e336134a0841c6c
DaQi <ydmmocoo@gmail.com>
2018-05-28 15:38:54 +0800

    增加其他题库

---
+++ app/src/main/java/com/ydmmocoo/androidinterview/ui/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.ydmmocoo.androidinterview.ui.activity;
+
+import com.ydmmocoo.androidinterview.R;
+import com.ydmmocoo.androidinterview.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected int getMainView() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+    }
+}
--- app/src/main/java/com/ydmmocoo/androidinterview/ui/activity/SubjectDetailActivity.java
+++ app/src/main/java/com/ydmmocoo/androidinterview/ui/activity/SubjectDetailActivity.java
@@ -10,10 +10,18 @@ import com.qmuiteam.qmui.widget.QMUITopBar;
 import com.socks.library.KLog;
 import com.ydmmocoo.androidinterview.R;
 import com.ydmmocoo.androidinterview.base.BaseActivity;
+import com.ydmmocoo.androidinterview.model.AlgorithmSubject;
 import com.ydmmocoo.androidinterview.model.AndroidSubject;
+import com.ydmmocoo.androidinterview.model.HRSubject;
 import com.ydmmocoo.androidinterview.model.JavaSubject;
+import com.ydmmocoo.androidinterview.model.NetworkSubject;
+import com.ydmmocoo.androidinterview.model.OpenSourceSubject;
+import com.ydmmocoo.androidinterview.ui.adapter.RvAlgorithmSubjectDetailAdapter;
 import com.ydmmocoo.androidinterview.ui.adapter.RvAndroidSubjectDetailAdapter;
+import com.ydmmocoo.androidinterview.ui.adapter.RvHRSubjectDetailAdapter;
 import com.ydmmocoo.androidinterview.ui.adapter.RvJavaSubjectDetailAdapter;
+import com.ydmmocoo.androidinterview.ui.adapter.RvNetworkSubjectDetailAdapter;
+import com.ydmmocoo.androidinterview.ui.adapter.RvOpenSourceSubjectDetailAdapter;
 
 import java.util.List;
 
@@ -35,6 +43,18 @@ public class SubjectDetailActivity extends BaseActivity {
     //Android题库
     private RvAndroidSubjectDetailAdapter mAndroidAdapter;
     private List<AndroidSubject> mAndroidList;
+    //开源
+    private RvOpenSourceSubjectDetailAdapter mOpenSourceAdapter;
+    private List<OpenSourceSubject> mOpenSourceList;
+    //网络
+    private RvNetworkSubjectDetailAdapter mNetworkAdapter;
+    private List<NetworkSubject> mNetworkList;
+    //算法
+    private RvAlgorithmSubjectDetailAdapter mAlgorithmAdapter;
+    private List<AlgorithmSubject> mAlgorithmList;
+    //HR
+    private RvHRSubjectDetailAdapter mHRAdapter;
+    private List<HRSubject> mHRList;
 
     private SnapHelper mSnapHelper;
 
@@ -66,13 +86,21 @@ public class SubjectDetailActivity extends BaseActivity {
             mRvContent.setAdapter(mAndroidAdapter);
             getAndroidData();
         } else if (title.contains("Android开源库面试题")) {
-
+            mOpenSourceAdapter = new RvOpenSourceSubjectDetailAdapter(R.layout.item_rv_subject_detail, mOpenSourceList);
+            mRvContent.setAdapter(mOpenSourceAdapter);
+            getOpenSourceData();
         } else if (title.contains("Android网络编程面试题")) {
-
+            mNetworkAdapter = new RvNetworkSubjectDetailAdapter(R.layout.item_rv_subject_detail, mNetworkList);
+            mRvContent.setAdapter(mNetworkAdapter);
+            getNetworkData();
         } else if (title.contains("数据结构与算法面试题")) {
-
+            mAlgorithmAdapter = new RvAlgorithmSubjectDetailAdapter(R.layout.item_rv_subject_detail, mAlgorithmList);
+            mRvContent.setAdapter(mAlgorithmAdapter);
+            getAlgorithmData();
         } else if (title.contains("HR面试题")) {
-
+            mHRAdapter = new RvHRSubjectDetailAdapter(R.layout.item_rv_subject_detail, mHRList);
+            mRvContent.setAdapter(mHRAdapter);
+            getHRData();
         } else {
             mJavaAdapter = new RvJavaSubjectDetailAdapter(R.layout.item_rv_subject_detail, mJavaList);
             mRvContent.setAdapter(mJavaAdapter);
@@ -122,6 +150,82 @@ public class SubjectDetailActivity extends BaseActivity {
                 if (e == null) {
                     mAndroidList = object;
                     mAndroidAdapter.setNewData(mAndroidList);
+                } else {
+                    KLog.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                }
+            }
+        });
+    }
+
+    private void getOpenSourceData() {
+        BmobQuery<OpenSourceSubject> query = new BmobQuery<OpenSourceSubject>();
+        //返回50条数据，如果不加上这条语句，默认返回10条数据
+        query.setLimit(50);
+        query.include("answer");
+        //执行查询方法
+        query.findObjects(new FindListener<OpenSourceSubject>() {
+            @Override
+            public void done(List<OpenSourceSubject> object, BmobException e) {
+                if (e == null) {
+                    mOpenSourceList = object;
+                    mOpenSourceAdapter.setNewData(mOpenSourceList);
+                } else {
+                    KLog.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                }
+            }
+        });
+    }
+
+    private void getNetworkData() {
+        BmobQuery<NetworkSubject> query = new BmobQuery<NetworkSubject>();
+        //返回50条数据，如果不加上这条语句，默认返回10条数据
+        query.setLimit(50);
+        query.include("answer");
+        //执行查询方法
+        query.findObjects(new FindListener<NetworkSubject>() {
+            @Override
+            public void done(List<NetworkSubject> object, BmobException e) {
+                if (e == null) {
+                    mNetworkList = object;
+                    mNetworkAdapter.setNewData(mNetworkList);
+                } else {
+                    KLog.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                }
+            }
+        });
+    }
+
+    private void getAlgorithmData() {
+        BmobQuery<AlgorithmSubject> query = new BmobQuery<AlgorithmSubject>();
+        //返回50条数据，如果不加上这条语句，默认返回10条数据
+        query.setLimit(50);
+        query.include("answer");
+        //执行查询方法
+        query.findObjects(new FindListener<AlgorithmSubject>() {
+            @Override
+            public void done(List<AlgorithmSubject> object, BmobException e) {
+                if (e == null) {
+                    mAlgorithmList = object;
+                    mAlgorithmAdapter.setNewData(mAlgorithmList);
+                } else {
+                    KLog.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                }
+            }
+        });
+    }
+
+    private void getHRData() {
+        BmobQuery<HRSubject> query = new BmobQuery<HRSubject>();
+        //返回50条数据，如果不加上这条语句，默认返回10条数据
+        query.setLimit(50);
+        query.include("answer");
+        //执行查询方法
+        query.findObjects(new FindListener<HRSubject>() {
+            @Override
+            public void done(List<HRSubject> object, BmobException e) {
+                if (e == null) {
+                    mHRList = object;
+                    mHRAdapter.setNewData(mHRList);
                 } else {
                     KLog.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
                 }
